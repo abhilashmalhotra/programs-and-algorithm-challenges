6518b0b682a8141dc6c3d5f158951a454e14029f
Abhilash <abhilashmalhotra17@gmail.com>
2020-03-22 16:14:21 +0530

    more solution on reverse string

--- codewars/reversed-string.js
+++ codewars/reversed-string.js
@@ -1,7 +1,25 @@
 // Complete the solution so that it reverses the string value passed into it.
 
-function solution(str) {
+/* --------- Solution 1 -------- */
+/* function solution(str) {
     return str.split('').reverse().join('');
+} */
+
+
+/* --------- Solution 2 -------- */
+/* function solution(str) {
+    let revStr = ''
+    let strArr = str.split('');
+    for (let i = strArr.length - 1; i >= 0; i--) {
+        revStr += strArr[i];
+    }
+    return revStr;
+} */
+
+
+/* --------- Solution 3 -------- */
+function solution(str) {
+    return str.split('').reduce((inital, next) => next + inital, '');
 }
 
 console.log(solution('world')); // returns 'dlrow'
